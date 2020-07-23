977a5e2ea32a4253e1cbffcd968045574d7a293f
Lyydiiaa <68226889+Lyydiiaa@users.noreply.github.com>
2020-07-23 12:49:41 +0200

    Add files via upload
    mi primera version

---
+++ paginawebveterinario/SVPAP - Nuestra Clínica veterinaria_files/codigo-post-cookies.php
@@ -0,0 +1,29 @@
+/*	window.fbAsyncInit = function() {
+    FB.init({
+      appId            : null,
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v2.9'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/es_ES/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));*/
+
+
+
+  
+    window.___gcfg = {lang: 'es_ES'};
+
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+  
